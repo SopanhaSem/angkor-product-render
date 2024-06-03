96010e105930adde0ee0f5140f45e1fe427dec9c
SopanhaSem <semsopanha072@gmail.com>
2024-06-03 18:47:05 +0700

    add: final

--- renderProducts/js/cardComponents.js
+++ renderProducts/js/cardComponents.js
@@ -1,9 +1,9 @@
 export const cardComponents = (product) => {
-  return ` <div class="relative flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96 m-20">
-   <div class="relative mx-4 mt-4 overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl h-96">
+  return ` <div class="flex flex-col text-gray-700 bg-white shadow-md bg-clip-border rounded-xl w-96 m-20 ">
+   <div class=" mx-4 mt-4 overflow-hidden text-gray-700 bg-white bg-clip-border rounded-xl h-96">
      <img
        src= ${
-         product.image ??
+         product.imageUrl ??
          "https://imgs.search.brave.com/YizlsYexGrY3kUsxE2vYkxBeSDFPTBt01qO13NHPI4Y/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzAxLzA5LzQwLzM0/LzM2MF9GXzEwOTQw/MzQ4MV9nOExiTWdp/YkJ4cm5sN0NCYzIw/MXdjSnNOaWhPV1RG/Mi5qcGc"
        }
        alt="card-image"
@@ -13,11 +13,11 @@ export const cardComponents = (product) => {
    <div class="p-6">
      <div class="flex items-center justify-between mb-2">
        <p class="block font-sans text-base antialiased font-medium leading-relaxed text-blue-gray-900">
-         ${product?.title || "Untitled"}
+         ${product?.name || "Untitled"}
        </p>
      </div>
      <p class="block font-sans text-md antialiased font-normal leading-normal text-gray-700 opacity-75">
-       ${"$" + product?.price || "Null"}
+       ${"$" + product?.originPrice || "Null"}
      </p>
    </div>
    <div class="p-6 pt-0">
--- renderProducts/js/scripts.js
+++ renderProducts/js/scripts.js
@@ -2,8 +2,14 @@ import { cardComponents } from "./cardComponents.js";
 import { product } from "../data/product.js";
 
 let cardContainer = document.querySelector("#cardComponents");
+const BASE_URL = "http://127.0.0.1:5500/renderProducts/data/data.json";
 
-product.forEach((product) => {
+fetch(BASE_URL).then((res)=> res.json()).then((data)=> {data.map((product)=>{
   let newCard = cardComponents(product);
   cardContainer.innerHTML += newCard;
-});
+})});
+
+// product.forEach((product) => {
+//   let newCard = cardComponents(product);
+//   cardContainer.innerHTML += newCard;
+// });
